cc3f0689ffb7af86fafd0b053723343fdb65eb6c
Markus <emmzet@web.de>
2021-11-10 12:08:34 +0100

    Add pages and components frontend final

--- backend/src/main/java/de/neuefische/backend/model/Location.java
+++ backend/src/main/java/de/neuefische/backend/model/Location.java
@@ -12,7 +12,10 @@ import lombok.NoArgsConstructor;
 
 public class Location {
 
+    private String name;
     private String id;
     private String city;
+    private String address;
+    private String maxCapacity;
 
 }
